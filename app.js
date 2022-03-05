f2df6ba2bd675979e7656277d637da85f5cc84d0
Phil Bailey <philbailey@msn.com>
2022-03-05 12:49:37 -0500

    work shop 2

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use("/users", usersRouter);
 
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
-app.use("/partners", campsiteRouter);
+app.use("/partners", partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
